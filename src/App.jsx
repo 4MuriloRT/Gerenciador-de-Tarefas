0d2b14835149cff3c89e217bef753e2feecc7618
Murilo Taborda <murilotaborda09@gmail.com>
2025-04-09 16:56:36 -0300

    Adiciona função onAddTaskSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 const App = ( ) => {
   const [tasks, setTasks] = useState([{
@@ -37,11 +38,21 @@ const App = ( ) => {
     setTasks(newTasks);
   }
 
+  const onAddTaskSubmit = (title, description) => {
+    const newTask = { 
+      id: v4(),
+      title: title,
+      description: description,
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 text-center font-bold"> Gerenciador de Tarefas </h1>
-        <AddTask/>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
         <Tasks 
           tasks={tasks}
           onTaskClick={onTaskClick}
